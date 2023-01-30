7347edf0a2fbf302e54a441e852ab3b878573f64
JohnDSmallpage <john.smallpage@correo.unimet.edu.ve>
2023-01-29 22:14:16 -0400

    Pruebas con js

---
+++ main.js
@@ -0,0 +1,7 @@
+const inputCity= document.getElementById("barra");
+const button= document.getElementById("boton");
+
+button.addEventListener("click", () => {
+    const value_input= inputCity.value;
+    
+})
